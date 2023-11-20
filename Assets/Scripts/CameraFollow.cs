3bf7ef586c4c4b2b0332a28ef5c6c2f3f2cf2e7d
GhettoNoodles <jcoetzee2201@gmail.com>
2023-11-20 13:16:32 +0200

    Basic Level
    textures, Player Controller, camera controls, and basic level

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,21 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Vector3 target;
-
-    private Vector3 _offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _offset = transform.position - target;
-    }
-
-    // Update is called once per frame
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform OrbitPoint;
     void Update()
     {
-        transform.position = new Vector3((target.x + _offset.x), (target.y + _offset.y), (target.z+ _offset.z));
-    }
+        OrbitPoint.position = target.position;
+        transform.LookAt(target.position);
+       }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float acceleration;
+    [SerializeField] private float maxVel;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform cameraOrbit;
+    [SerializeField] private Transform parent;
+    [SerializeField] private float xsens;
+    [SerializeField] private float ysens;
+    private bool _grounded;
+    private float _lookX;
+    private float _lookY;
+    private float _xRot;
+    private float _yRot;
+    private Vector3 _movement;
+    private Vector3 _adjustedMovement;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _grounded = Physics.Raycast(transform.position, Vector3.down, 2.8f);
+        if ((Input.GetButtonDown("Jump")) && _grounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.z = Input.GetAxis("Vertical");
+        _lookY = Input.GetAxis("Mouse X") * Time.deltaTime * xsens * 1000;
+        _lookX = Input.GetAxis("Mouse Y") * Time.deltaTime * ysens * 1000;
+        _yRot += _lookY;
+        _xRot -= _lookX;
+        _xRot = Mathf.Clamp(_xRot, -90f, 90f);
+        cam.rotation = Quaternion.Euler(_xRot, 0, 0);
+        cameraOrbit.rotation = Quaternion.Euler(0, _yRot, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        parent.forward = new Vector3(cam.forward.x, 0, cam.forward.z);
+        _adjustedMovement = parent.TransformDirection(_movement);
+        if (rb.velocity.magnitude < maxVel)
+        {
+            rb.AddForce(_adjustedMovement * acceleration, ForceMode.Acceleration);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Danger"))
+        {
+            Debug.Log("Dead");
+        }
     }
 }
